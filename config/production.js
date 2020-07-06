418ad4d9c3efcb113af00af4860d67e39232a8c8
Nayan Jyoti Das <nayanj@geekyants.com>
2020-07-06 17:13:10 +0530

    Fix env vars in production.js

--- config/production.js
+++ config/production.js
@@ -2,10 +2,10 @@ require("dotenv/config");
 
 const {
   DEV_DB_DIALECT,
-  DEV_JWT_AUDIENCE,
-  DEV_JWT_ISSUER,
-  DEV_JWT_ALGORITHM,
-  DEV_JWT_EXPIRES_IN,
+  JWT_AUDIENCE,
+  JWT_ISSUER,
+  JWT_ALGORITHM,
+  JWT_EXPIRES_IN,
   DATABASE_URL,
   DEV_AWS_SES_SENDER,
   DEV_APP_DOMAIN,
@@ -18,10 +18,10 @@ module.exports = {
   databaseUrl: DATABASE_URL,
   dialect: DEV_DB_DIALECT,
   jwtOptions: {
-    audience: DEV_JWT_AUDIENCE,
-    issuer: DEV_JWT_ISSUER,
-    algorithm: DEV_JWT_ALGORITHM,
-    expiresIn: DEV_JWT_EXPIRES_IN,
+    audience: JWT_AUDIENCE,
+    issuer: JWT_ISSUER,
+    algorithm: JWT_ALGORITHM,
+    expiresIn: JWT_EXPIRES_IN,
   },
   //   s3: {
   //     bucketName: DEV_AWS_S3_BUCKET_NAME,
