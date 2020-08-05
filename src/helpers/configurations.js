a3e15bc2aedf695428384176e1b8b12946930af5
Nayan Jyoti Das <nayanj@geekyants.com>
2020-08-05 20:32:40 +0530

    Add jwt secret to config file

--- src/helpers/configurations.js
+++ src/helpers/configurations.js
@@ -2,6 +2,7 @@ const config = require("config");
 
 module.exports = {
   jwtOptions: {
+    secretKey: config.get("jwtOptions.secretKey"),
     audience: config.get("jwtOptions.audience"),
     issuer: config.get("jwtOptions.issuer"),
     algorithm: config.get("jwtOptions.algorithm"),
